2acfdc464052e306d3312cbf90ac40f3390117f4
giles <gilesknap@gmail.com>
2019-03-16 16:18:29 +0000

    new structure

--- bookbot/caversham.py
+++ bookbot/caversham.py
@@ -5,24 +5,27 @@ from flask import Flask
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions, ActionChains
 from selenium.common.exceptions import (WebDriverException,
-                                        StaleElementReferenceException)
-
+                                        StaleElementReferenceException,
+                                        TimeoutException)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 app = Flask(__name__)
 
 CHROME_DRIVER_PATH = 'chromedriver'
 SITE_URL = 'https://indma01.clubwise.com/caversham/index.html'
 driver: webdriver = None
+actions = None
 
 
 def click_element(path):
     result = True
     sleep(.2)
-    ac = ActionChains(driver)
     try:
         element = driver.find_element_by_xpath(path)
         if element:
-            ac.move_to_element(element).move_by_offset(
+            actions.move_to_element(element).move_by_offset(
                 0, 0).click().perform()
     except (WebDriverException, StaleElementReferenceException) as e:
         app.logger.error("click fail %s", e)
@@ -30,66 +33,66 @@ def click_element(path):
     return result
 
 
-def check_text(text, tag='*'):
-    path = '//{1}[text()="{0}"]'.format(text, tag)
-    return driver.find_elements_by_xpath(path) != []
+def click(path, check_text, retries: int = 5):
+    check_path = '//*[text()="{0}"]'.format(check_text)
+    repeat = 0
+    while repeat < retries:
+        if click_element(path):
+            wait = WebDriverWait(driver, 2)
+            try:
+                wait.until(
+                    EC.presence_of_element_located(
+                        (By.XPATH, check_path)))
+                break
+            except TimeoutException:
+                app.logger.warning("retrying {}".format(path))
+        repeat += 1
+    if retries == repeat:
+        raise RuntimeError('Click on {} failed to find {}', path, check_path)
 
 
-def click_text(text, tag='*'):
+def click_button():
+    path = ''
+
+
+def click_text(text: str, check_text: str, tag: str = '*', retries: int = 5):
     path = '//{1}[text()="{0}"]'.format(text, tag)
-    return click_element(path)
+    click(path, check_text, retries)
 
 
 def site_login():
     global driver
+    global actions
     options = ChromeOptions()
     # options.headless = True
     driver = webdriver.Chrome(CHROME_DRIVER_PATH,
                           options=options)
-
+    actions = ActionChains(driver)
     driver.implicitly_wait(2)
     driver.get(SITE_URL)
 
     element = driver.find_element_by_name('oLoginName')
     element.send_keys("tishtashpalmer@gmail.com")
     element = driver.find_element_by_name('oPassword')
-    element.send_keys('Spider00')
+    element.send_keys('***')
 
-    # Holy mackerel !! (could be refactored)
-    step = 1
-    for _ in range(10):
-        if step == 1:
-            click_text("Sign In")
-            if check_text("Continue"):
-                step += 1
-            else:
-                continue
-        if step == 2:
-            click_text("Continue")
-            if check_text("Make a Booking"):
-                step += 1
-            else:
-                continue
-        if step == 3:
-            click_text("Make a Booking")
-            if check_text("Book a Class"):
-                step += 1
-            else:
-                continue
-        if step == 4:
-            click_text("Book a Class")
-            if check_text("Select a time to book"):
-                break
-            else:
-                continue
-        app.logger.warning('retrying step %d', step)
+    click_text("Sign In", "Continue")
 
-    title = driver.find_element_by_class_name("DiaryHeading")
-    app.logger.info("got to page %s", title.text)
+
+def return_to_classes():
+    # driver.refresh()
+    click_text("Continue", "Make a Booking")
+    click_text("Make a Booking", "Book a Class")
+    click_text("Book a Class", "Select a time to book")
+
+    # this is the home b
+    # data-serveronclick="back-click"
 
 
 # for individual testing of this module
 if __name__ == "__main__":
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    # app.logger.addHandler(logging.StreamHandler(sys.stdout))
+    app.logger.setLevel(logging.INFO)
     site_login()
 
+    return_to_classes()
